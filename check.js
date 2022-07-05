f03a37f5b62fc58134d2e7682cec209e34cb778f
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-07-05 16:01:38 +0800

    rechange

--- check.js
+++ check.js
@@ -3,8 +3,7 @@
     function getParameters() {
         const searchParams = new URLSearchParams(window.location.search);
         var productId=searchParams.get("product");
-        var url="intent://theindemo.github.io/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
+        var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
         document.getElementById("q").value=productId;
           window.location.replace(url);
     }
-
