9796c6c929dc30894cdd79e3b79226fd00dbbaac
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-07-05 23:05:02 +0800

    rechange

--- check.js
+++ check.js
@@ -4,6 +4,5 @@
         const searchParams = new URLSearchParams(window.location.search);
         var productId=searchParams.get("product");
         var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
-        document.getElementById("q").value=productId;
           window.location.replace(url);
     }
