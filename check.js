75445fe8f89d7bd36501e5aa5752131294a378ee
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-07-06 10:38:12 +0800

    change check.js

--- check.js
+++ check.js
@@ -1,8 +1,12 @@
+function getParameters() {
+  const searchParams = new URLSearchParams(window.location.search);
+  var productId=searchParams.get("product");
+  var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
+  window.location.replace(url);
+  setTimeout(hideLoading, 500);
+}
 
-
-    function getParameters() {
-        const searchParams = new URLSearchParams(window.location.search);
-        var productId=searchParams.get("product");
-        var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;package=com.tta.qr;end";
-          window.location.replace(url);
-    }
+function hideLoading(){
+  var element = document.getElementById("page_loader");
+  element.style.display = "none";
+}
