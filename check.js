f98652d5e1577ca9fa9aa3f66cc6f604cdfccc90
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-07-20 17:39:01 +0800

    change UI in web

--- check.js
+++ check.js
@@ -3,10 +3,9 @@ function getParameters() {
   var productId=searchParams.get("product");
   var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
   window.location.replace(url);
-  setTimeout(hideLoading, 500);
+  setTimeout(goToPlayStore, 500);
 }
 
-function hideLoading(){
-  var element = document.getElementById("page_loader");
-  element.style.display = "none";
+function goToPlayStore(){
+    window.location.replace("https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.github.android");
 }
