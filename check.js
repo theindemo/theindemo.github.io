e6a2e59e3c59130b17ba4c83088365d1463638fe
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-07-21 17:35:04 +0800

    checking ios or android

--- check.js
+++ check.js
@@ -1,11 +1,25 @@
 function getParameters() {
   const searchParams = new URLSearchParams(window.location.search);
   var productId=searchParams.get("product");
-  var url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
-  window.location.replace(url);
+    
+    if(isIOSDevice()){
+        document.write("I am an IOS device!");
+        var ios_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
+        window.location.replace(ios_url);
+        
+    }else{
+        document.write("I am an android device!");
+        var android_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
+        window.location.replace(android_url);
+    }
+    
   setTimeout(goToPlayStore, 500);
 }
 
 function goToPlayStore(){
     window.location.replace("https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.github.android");
+}
+
+function isIOSDevice(){
+   return !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 }
