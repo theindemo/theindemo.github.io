41f204b2668666302325b8774369b53a3e1b7758
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-01 18:12:30 +0800

    Change timeout

--- check.js
+++ check.js
@@ -13,7 +13,7 @@ function getParameters() {
         window.location.replace(android_url);
     }
     
-  setTimeout(goToPlayStore, 1000000);
+  setTimeout(goToPlayStore, 10000000);
 }
 
 function goToPlayStore(){
