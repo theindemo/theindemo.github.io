d2981664dcd5bbc160867aa3d776b3b3bb488417
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-02 16:41:48 +0800

    change index ui

--- check.js
+++ check.js
@@ -3,12 +3,10 @@ function getParameters() {
   var productId=searchParams.get("product");
   
     if(isIOSDevice()){
-        document.write("I am an IOS device!");
         var ios_url="hostapp:/message?string="+productId+"#Intent;scheme=tta;end";
         window.location.replace(ios_url);
         
     }else{
-        document.write("I am an android device!");
         var android_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
         window.location.replace(android_url);
     }
@@ -17,19 +15,11 @@ function getParameters() {
 }
 
 function onVisibilityChange() {
-    document.write("onVisibilityChange")
   if (document.visibilityState === 'visible') {
-      document.write("user is focused on the page")
       window.location = "https://theindemo.github.io/pages/download.html";
   } else {
       document.write("user left the page")
   }
-}
-
-function makeDelay(){
-    document.write("makeDelay")
-    setTimeout(getParameters, 100);
-   
 }
 
 
