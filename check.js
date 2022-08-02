6bd34641101d855a223aa262447fb82b8a2784ac
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-02 15:45:27 +0800

    change index ui

--- check.js
+++ check.js
@@ -1,27 +1,17 @@
 function getParameters() {
   const searchParams = new URLSearchParams(window.location.search);
   var productId=searchParams.get("product");
-    
   document.addEventListener('visibilitychange', onVisibilityChange);
-    
     if(isIOSDevice()){
-        /document.write("I am an IOS device!");
+        document.write("I am an IOS device!");
         var ios_url="hostapp:/message?string="+productId+"#Intent;scheme=tta;end";
-        //window.location.replace(ios_url);
-//        setTimeout(function () { window.location = ios_url; }, 500);
-//        window.location = "https://theindemo.github.io/pages/download.html";
+        window.location.replace(ios_url);
         
     }else{
         document.write("I am an android device!");
         var android_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
         window.location.replace(android_url);
-        //setTimeout(window.location.replace(android_url), 1500);
-        
     }
-    
-// setTimeout(hideLoading, 500);
-// hideLoading()
-    
 }
 
 function onVisibilityChange() {
@@ -33,34 +23,6 @@ function onVisibilityChange() {
   }
 }
 
-//setTimeout(function () { window.location = "https://itunes.apple.com/appdir"; }, 25);
-
-function checking(){
-    setTimeout(hideLoading, 11500);
-    //window.location = "https://theindemo.github.io/pages/download.html";
-    //window.location = "appname://";
-    //window.location.replace("https://theindemo.github.io/pages/download.html");
-}
-
-function hideLoading(){
-//  var element = document.getElementById("page_loader");
-//  element.style.display = "none";
-//  document.getElementById("page_loader").style.display = 'none';
-    document.write("hideloading")
-}
-
-//function goToPlayStore(){
-//
-//    if(isIOSDevice()){
-//        //document.write("I am an IOS device!");
-//        window.location.replace("https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.github.android");
-//
-//    }else{
-//        //document.write("I am an android device!");
-//        window.location.replace("https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.github.android");
-//    }
-//
-//}
 
 function isIOSDevice(){
    return !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
