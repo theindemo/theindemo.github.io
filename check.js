e016875c9216af0d982c72772d55a1045784528e
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-02 14:13:24 +0800

    change index ui

--- check.js
+++ check.js
@@ -7,7 +7,7 @@ function getParameters() {
         var ios_url="hostapp:/message?string="+productId+"#Intent;scheme=tta;end";
         //window.location.replace(ios_url);
         
-        setTimeout(function () { window.location = ios_url; }, 25);
+        setTimeout(function () { window.location = ios_url; }, 500);
         window.location = "https://theindemo.github.io/pages/download.html";
         
         
@@ -16,7 +16,7 @@ function getParameters() {
         var android_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
         //window.location.replace(android_url);
         
-        setTimeout(function () { window.location = android_url; }, 25);
+        setTimeout(function () { window.location = android_url; }, 500);
         window.location = "https://theindemo.github.io/pages/download.html";
     }
     
