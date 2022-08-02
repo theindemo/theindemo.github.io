43e09d29d1518100fa51de8eaba3140782e8eb3c
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-02 16:26:27 +0800

    change index ui

--- check.js
+++ check.js
@@ -12,7 +12,8 @@ function getParameters() {
         var android_url="intent://theinqr.com/?product="+productId+"#Intent;scheme=tta;end";
         window.location.replace(android_url);
     }
-    document.addEventListener('visibilitychange', onVisibilityChange);
+
+    setTimeout(onVisibilityChange, 500);
 }
 
 function onVisibilityChange() {
