cbcf6ffc173ea3e9a8eb29ac0db923543eefaf48
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2022-08-02 17:07:11 +0800

    change index ui

--- check.js
+++ check.js
@@ -1,5 +1,5 @@
 function getParameters() {
-    setTimeout(prepareView, 20000);
+    setTimeout(prepareView, 10000);
 }
 
 function prepareView() {
@@ -15,7 +15,7 @@ function prepareView() {
         window.location.replace(android_url);
     }
 
-    setTimeout(onVisibilityChange, 10000);
+    setTimeout(onVisibilityChange, 1000);
 }
 
 
