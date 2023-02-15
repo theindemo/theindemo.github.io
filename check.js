1262fb99bd6ab2b88dc2930f4134422f5dfa5c2c
Thein Tun Aung <theintunaung@Theins-MacBook-Pro.local>
2023-02-15 18:02:20 +0800

    add langauge

--- check.js
+++ check.js
@@ -44,27 +44,27 @@ function detectLang(){
               pta2label1="We are currently checking if the Philips TV Remote application is installed on your device.";
               pta2label2="You will automatically proceed to the Philips TV Remote application if it is already installed.";
           break;
-        case "de":
+        case "de-de":
               pta2label1="Wir prüfen derzeit, ob die Philips TV Remote App auf Ihrem Gerät installiert ist.";
               pta2label2="Sie gelangen automatisch zur Philips TV Remote App, wenn diese bereits installiert ist.";
           break;
-        case "nl":
+        case "nl-nl":
               pta2label1="We controleren momenteel of de Philips TV Remote App op uw apparaat is geïnstalleerd.";
               pta2label2="U wordt automatisch doorgestuurd naar de Philips TV Remote App als deze al is geïnstalleerd.";
           break;
-        case "es":
+        case "es-es":
               pta2label1="Estamos comprobando si la aplicación Philips TV Remote está instalada en su dispositivo.";
               pta2label2="Si ya está instalada, accederá automáticamente a la aplicación Philips TV Remote.";
           break;
-        case "fr":
+        case "fr-fr":
               pta2label1="Nous vérifions actuellement si l'application Philips TV Remote est installée sur votre appareil.";
               pta2label2="Vous pourrez accéder automatiquement à l'application Philips TV Remote si elle est déjà installée.";
           break;
-        case "it":
+        case "it-it":
               pta2label1="È in corso la verifica dell'installazione dell'applicazione Philips TV Remote sul dispositivo.";
               pta2label2="Se è già installata, si procede automaticamente all'applicazione Philips TV Remote.";
           break;
-        case "pt":
+        case "pt-pt":
               pta2label1="Neste momento, estamos a verificar se a aplicação Philips TV Remote está instalada no seu dispositivo.";
               pta2label2="Se já estiver instalada, a aplicação Philips TV Remote será automaticamente aberta.";
           break;
